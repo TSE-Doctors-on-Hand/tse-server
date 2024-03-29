a15c31941adf1c7ca12afe575dbd94d08f3436a2
BSpoones <ben.soones@gmail.com>
2024-03-29 20:50:18 +0000

    feat: Practice storage

---
+++ src/main/kotlin/cmp2804/tse/server/storage/practices/Practice.kt
@@ -0,0 +1,37 @@
+package cmp2804.tse.server.storage.practices
+
+import javax.persistence.*
+
+const val PRACTICE_TABLE_NAME = "practices"
+
+/**
+ * **Practices table**
+ *
+ * Stores all information about a practice
+ *
+ * @author Ben Soones
+ */
+@Entity
+@Table(name = PRACTICE_TABLE_NAME)
+data class Practice(
+    @Id
+    @GeneratedValue
+    val id: Long,
+
+    /**
+     * Practice name
+     *
+     * This will appear on the search list
+     */
+    val name: String,
+
+    /**
+     * Practice Location
+     *
+     * A Latitude and Longitude pair showing the GPS co-ordinates of the practice
+     * This can be used with a maps API to get the location
+     */
+    val location: Pair<Double, Double>,
+) {
+    constructor() : this(0, "", Pair(0.00, 0.00))
+}
