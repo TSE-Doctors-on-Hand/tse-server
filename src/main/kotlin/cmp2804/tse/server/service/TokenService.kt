6108275e42649686349829def652b876dea292d5
BSpoones <76079263+BSpoones@users.noreply.github.com>
2024-05-01 13:52:06 +0100

    feat: TokenService

---
+++ src/main/kotlin/cmp2804/tse/server/service/TokenService.kt
@@ -0,0 +1,39 @@
+package cmp2804.tse.server.service
+
+import cmp2804.tse.server.storage.users.User
+import org.springframework.security.oauth2.jwt.*
+import org.springframework.stereotype.Service
+import java.time.Instant
+import java.time.temporal.ChronoUnit
+
+@Service
+class TokenService(
+    private val jwtDecoder: JwtDecoder,
+    private val jwtEncoder: JwtEncoder,
+    private val userService: UserService,
+) {
+
+    fun createToken(user: User): String {
+        val jwsHeader = JwsHeader.with { "HS256" }.build()
+        val claims = JwtClaimsSet.builder()
+            .issuedAt(Instant.now())
+            .expiresAt(Instant.now().plus(30L, ChronoUnit.DAYS))
+            .subject(user.username)
+            .claim("userId", user.id)
+            .build()
+        return jwtEncoder.encode(JwtEncoderParameters.from(jwsHeader, claims)).tokenValue
+    }
+
+    fun parseToken(token: String): User? {
+        return try {
+            val jwt = jwtDecoder.decode(token)
+            val userId = jwt.claims["userId"] as Long
+            userService.findById(userId)
+        } catch (e: Exception) {
+            null
+        }
+    }
+
+
+
+}
