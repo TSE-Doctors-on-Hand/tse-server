c41b38bc372b95b35263af695468c92fb364b894
BSpoones <76079263+BSpoones@users.noreply.github.com>
2024-05-01 14:09:49 +0100

    feat: Symptom Matcher

---
+++ src/main/kotlin/cmp2804/tse/server/util/matcher/SymptomMatcher.kt
@@ -0,0 +1,45 @@
+package cmp2804.tse.server.util.matcher
+
+import cmp2804.tse.server.storage.doctors.Doctor
+import cmp2804.tse.server.storage.patients.Patient
+import cmp2804.tse.server.storage.symptoms.Symptom
+
+object SymptomMatcher {
+    fun matchSymptoms(
+        patient: Patient,
+        symptoms: List<Symptom>
+        ) {
+        // Get all doctors
+        // Filter by symptom
+
+    }
+
+    fun matchDoctors(
+        doctors: List<Doctor>,
+        symptoms: List<Symptom>
+        ){
+
+    }
+
+    fun matchDoctor(
+        doctor: Doctor,
+        symptoms: Set<Symptom>
+    ): MatchedDoctor {
+        val similarity: Double
+
+        val doctorSymptoms = doctor.symptoms
+        val intersection = doctorSymptoms.intersect(symptoms)
+        val union = doctorSymptoms.union(symptoms)
+
+        similarity = if (union.isEmpty()) {
+            100.00
+        } else {
+            (intersection.size.toDouble() / union.size.toDouble()) * 100
+        }
+
+
+    }
+
+
+
+}
