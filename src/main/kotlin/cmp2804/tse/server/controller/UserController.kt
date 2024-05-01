93a6d5372618012d09888bc0d0c0cf34bebb7b32
BSpoones <76079263+BSpoones@users.noreply.github.com>
2024-05-01 13:55:26 +0100

    add: Post user

--- src/main/kotlin/cmp2804/tse/server/controller/UserController.kt
+++ src/main/kotlin/cmp2804/tse/server/controller/UserController.kt
@@ -7,6 +7,8 @@ import org.springframework.http.ResponseEntity
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -22,4 +24,13 @@ class UserController(private val usersRepository: UsersRepository) {
     ): ResponseEntity<User> {
         return ResponseUtils.getEntryResponseById(usersRepository, userId)
     }
+
+    @PostMapping("/")
+    fun postUser(
+        @RequestBody
+        user: User
+    ) {
+        throw NotImplementedError()
+    }
+
 }
