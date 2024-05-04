70936253baf56ab91824097bf81acae915ece4e7
BSpoones <76079263+BSpoones@users.noreply.github.com>
2024-05-04 12:51:56 +0100

    refactor: Using services instead of repository

--- src/main/kotlin/cmp2804/tse/server/util/ResponseUtils.kt
+++ src/main/kotlin/cmp2804/tse/server/util/ResponseUtils.kt
@@ -1,12 +1,14 @@
 package cmp2804.tse.server.util
 
+import cmp2804.tse.server.service.base.BaseService
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.http.ResponseEntity
 
 object ResponseUtils {
 
     // TODO -> Javadoc
-    fun <T> getEntryResponseById(repository: JpaRepository<T, Long>, id: Long): ResponseEntity<T> {
-        return repository.findById(id).map { ResponseEntity.ok(it) }.orElse(ResponseEntity.notFound().build())
+    fun <T> getEntryResponseById(service: BaseService<T, Long>, id: Long): ResponseEntity<T> {
+        return service.getRepository().findById(id).map { ResponseEntity.ok(it) }
+            .orElse(ResponseEntity.notFound().build())
     }
 }
