fdc5423091a423edd1670a50ad8d04a41978f834
BSpoones <ben.soones@gmail.com>
2024-05-04 19:11:53 +0100

    feat: Past timestamp validator

---
+++ src/main/kotlin/cmp2804/tse/server/storage/validators/pasttimestamp/PastTimestamp.kt
@@ -0,0 +1,14 @@
+package cmp2804.tse.server.storage.validators.pasttimestamp
+
+import jakarta.validation.Constraint
+import jakarta.validation.Payload
+import kotlin.reflect.KClass
+
+@Target(AnnotationTarget.FIELD, AnnotationTarget.TYPE)
+@Retention(AnnotationRetention.RUNTIME)
+@Constraint(validatedBy = [PastTimestampValidator::class])
+annotation class PastTimestamp(
+    val message: String = "Must be a time in the past",
+    val groups: Array<KClass<*>> = [],
+    val payload: Array<KClass<out Payload>> = []
+)
---
+++ src/main/kotlin/cmp2804/tse/server/storage/validators/pasttimestamp/PastTimestampValidator.kt
@@ -0,0 +1,11 @@
+package cmp2804.tse.server.storage.validators.pasttimestamp
+
+import jakarta.validation.ConstraintValidator
+import jakarta.validation.ConstraintValidatorContext
+
+class PastTimestampValidator : ConstraintValidator<PastTimestamp, Long> {
+    override fun isValid(value: Long?, context: ConstraintValidatorContext): Boolean {
+        if (value == null) return true // Consider null values as valid
+        return value < System.currentTimeMillis() // Validate if it's in the future
+    }
+}
