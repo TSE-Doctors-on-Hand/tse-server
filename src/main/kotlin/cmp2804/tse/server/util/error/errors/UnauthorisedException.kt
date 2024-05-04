4a0351819814cd83e67e8285908b0931f035df7c
BSpoones <ben.soones@gmail.com>
2024-05-04 14:24:58 +0100

    feat: Sign out

---
+++ src/main/kotlin/cmp2804/tse/server/util/error/errors/UnauthorisedException.kt
@@ -0,0 +1,8 @@
+package cmp2804.tse.server.util.error.errors
+
+/**
+ * Exception can be called whenever a request is unauthorised
+ *
+ * @author Ben Soones
+ */
+class UnauthorisedException(message: String): Exception(message)
