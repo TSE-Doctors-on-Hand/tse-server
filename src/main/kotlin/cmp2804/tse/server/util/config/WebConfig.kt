b85b55daee7443743a0a9fcaf274abcd8ad97e27
BSpoones <76079263+BSpoones@users.noreply.github.com>
2024-05-04 12:45:37 +0100

    add: Web config

---
+++ src/main/kotlin/cmp2804/tse/server/util/config/WebConfig.kt
@@ -0,0 +1,29 @@
+package cmp2804.tse.server.util.config
+
+import cmp2804.tse.server.util.auth.UserArgumentResolver
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.method.support.HandlerMethodArgumentResolver
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+@EnableWebMvc
+class WebConfig(
+    private val userArgumentResolver: UserArgumentResolver
+): WebMvcConfigurer {
+
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins(
+                "http://localhost:3000",
+                "http://localhost:8080",
+                "http://localhost:4200"
+            )
+            .allowCredentials(true)
+    }
+
+    override fun addArgumentResolvers(resolvers: MutableList<HandlerMethodArgumentResolver>) {
+        resolvers.add(userArgumentResolver)
+    }
+}
