4c3a83b0689841e9d38abf43a32a91a0937f9bcd
BSpoones <ben.soones@gmail.com>
2024-05-04 18:04:43 +0100

    fix: Set to int instead of enum

--- src/main/kotlin/cmp2804/tse/server/util/request/SignUpRequest.kt
+++ src/main/kotlin/cmp2804/tse/server/util/request/SignUpRequest.kt
@@ -1,14 +1,12 @@
 package cmp2804.tse.server.util.request
 
-import cmp2804.tse.server.storage.users.SexEnum
-
 data class SignUpRequest(
     val username: String,
     val password: String,
     val forename: String,
     val surname: String,
     val dateOfBirth: Long,
-    val sex: SexEnum,
+    val sex: Int,
     val pronouns: MutableList<String>,
     val email: String,
     val phone: String?,
