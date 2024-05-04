7d1664fff3c4853d6a79ecf5771c2be01406f28c
BSpoones <76079263+BSpoones@users.noreply.github.com>
2024-05-04 12:44:53 +0100

    add: Security exception

--- src/main/kotlin/cmp2804/tse/server/Main.kt
+++ src/main/kotlin/cmp2804/tse/server/Main.kt
@@ -1,11 +1,13 @@
 package cmp2804.tse.server
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration
 import org.springframework.boot.runApplication
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
-@SpringBootApplication
+
+@SpringBootApplication(exclude = [SecurityAutoConfiguration::class])
 class DoctorsOnHand
 
 fun main(args: Array<String>) {
