c29460324b64912297302c18a9992277cd05058b
BSpoones <ben.soones@gmail.com>
2024-05-04 18:03:47 +0100

    add: Protected endpoints

--- src/main/kotlin/cmp2804/tse/server/util/auth/AuthenticationFilter.kt
+++ src/main/kotlin/cmp2804/tse/server/util/auth/AuthenticationFilter.kt
@@ -7,8 +7,16 @@ import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import org.springframework.stereotype.Component
 
+
 @Component
 class AuthenticationFilter(private val authService: AuthService) : Filter {
+
+    private val PROTECTED_ENDPOINTS = listOf(
+        "/api/appointment/*",
+        "/api/doctor/*",
+        "/api/user/*"
+    )
+
     override fun doFilter(
         request: ServletRequest,
         response: ServletResponse,
@@ -18,13 +26,19 @@ class AuthenticationFilter(private val authService: AuthService) : Filter {
             val token = request.cookies?.firstOrNull { it.name == TOKEN_COOKIE_NAME }?.value
             val user = authService.getUser(token)
 
-            if (user != null) {
-                // Attach user information to the request
-                request.setAttribute("user", user)
-            } else {
-                (response as HttpServletResponse).status = HttpServletResponse.SC_UNAUTHORIZED
-                response.writer.write("Unauthorised")
-                return
+            val isProtectedEndpoint = PROTECTED_ENDPOINTS.any { endpoint ->
+                request.requestURI.startsWith(endpoint)
+            }
+
+            if (isProtectedEndpoint) {
+                if (user != null) {
+                    // Attach user information to the request
+                    request.setAttribute("user", user)
+                } else {
+                    (response as HttpServletResponse).status = HttpServletResponse.SC_UNAUTHORIZED
+                    response.writer.write("Unauthorised")
+                    return
+                }
             }
         }
         chain.doFilter(request, response) // Continue the filter chain
