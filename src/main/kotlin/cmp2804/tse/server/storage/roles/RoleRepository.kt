b784b1776a06a76ac116bf6cd711139903ae45d9
BSpoones <76079263+BSpoones@users.noreply.github.com>
2024-05-04 12:46:13 +0100

    add: Roles

---
+++ src/main/kotlin/cmp2804/tse/server/storage/roles/RoleRepository.kt
@@ -0,0 +1,12 @@
+package cmp2804.tse.server.storage.roles
+
+import org.springframework.data.jpa.repository.JpaRepository
+
+/**
+ * Repository interface for role storage
+ *
+ * @param [Role] entity type
+ * @param [Long] ID type
+ * @author Ben Soones
+ */
+interface RoleRepository : JpaRepository<Role, Long> {}
---
+++ src/main/kotlin/cmp2804/tse/server/storage/roles/RolesEnum.kt
@@ -0,0 +1,7 @@
+package cmp2804.tse.server.storage.roles
+
+enum class RolesEnum {
+    PATIENT,
+    DOCTOR,
+    ADMIN;
+}
