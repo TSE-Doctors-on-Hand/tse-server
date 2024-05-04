df54fced87740172d67faf90f18f84418bbde04e
BSpoones <ben.soones@gmail.com>
2024-05-04 18:04:28 +0100

    add: Unauthorised status code

--- src/main/kotlin/cmp2804/tse/server/util/error/errors/UnauthorisedException.kt
+++ src/main/kotlin/cmp2804/tse/server/util/error/errors/UnauthorisedException.kt
@@ -1,8 +1,12 @@
 package cmp2804.tse.server.util.error.errors
 
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.ResponseStatus
+
 /**
  * Exception can be called whenever a request is unauthorised
  *
  * @author Ben Soones
  */
+@ResponseStatus(value = HttpStatus.UNAUTHORIZED)
 class UnauthorisedException(message: String): Exception(message)
