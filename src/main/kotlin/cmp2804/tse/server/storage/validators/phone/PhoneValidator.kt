4e98588ffb07a8db2872b616758adc1b8e5e911c
Oliver Whitehead <xfawks@gmail.com>
2024-05-06 18:11:36 +0100

    feat: add - support into phone number regex

--- src/main/kotlin/cmp2804/tse/server/storage/validators/phone/PhoneValidator.kt
+++ src/main/kotlin/cmp2804/tse/server/storage/validators/phone/PhoneValidator.kt
@@ -5,7 +5,7 @@ import jakarta.validation.ConstraintValidatorContext
 
 class PhoneValidator : ConstraintValidator<Phone, String> {
 
-    private val phoneRegex = "(?:\\+44|0) ?(?:\\d{3} ?\\d{3} ?\\d{4}|\\d{4} ?\\d{3} ?\\d{3}|\\d{2} ?\\d{4} ?\\d{4})".toRegex()
+    private val phoneRegex = "(?:\\+44|0) ?(?:\\d{3} ?\\d{3} ?\\d{4}|\\d{4} ?\\d{3} ?\\d{3}|\\d{2} ?\\d{4} ?\\d{4}|\\d{4}-\\d{3}-\\d{3})".toRegex()
 
     override fun isValid(value: String?, context: ConstraintValidatorContext): Boolean {
         if (value == null) return true // Consider null values as valid
