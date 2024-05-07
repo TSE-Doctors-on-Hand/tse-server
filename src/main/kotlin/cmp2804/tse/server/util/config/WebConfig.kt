c5ba7bda6e66bcb03d6d958f44fcaf3de63e9086
Oliver Whitehead <xfawks@gmail.com>
2024-05-07 23:57:26 +0100

    fix: new cors registry

--- src/main/kotlin/cmp2804/tse/server/util/config/WebConfig.kt
+++ src/main/kotlin/cmp2804/tse/server/util/config/WebConfig.kt
@@ -1,6 +1,12 @@
 package cmp2804.tse.server.util.config
 
+import cmp2804.tse.server.util.auth.UserArgumentResolver
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.Customizer
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.web.SecurityFilterChain
 import org.springframework.web.method.support.HandlerMethodArgumentResolver
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.EnableWebMvc
@@ -8,22 +14,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
 @EnableWebMvc
-class WebConfig: WebMvcConfigurer {
+class WebConfig(
+    private val userArgumentResolver: UserArgumentResolver
+): WebMvcConfigurer {
 
     override fun addCorsMappings(registry: CorsRegistry) {
-        registry.addMapping("/**")
-            .allowedOrigins(
-                "http://localhost:3000",
-                "http://localhost:8080",
-                "http://localhost:4200"
-            )
-            .allowedMethods("GET", "POST", "PUT", "DELETE")
-            .allowedHeaders("Content-Type", "Authorization", "Access-Control-Allow-Origin") // Allow necessary headers
-            .allowCredentials(true)
+        registry.addMapping("/**").allowedOrigins("http://localhost:3000").allowedMethods("*")
     }
 
 
     override fun addArgumentResolvers(resolvers: MutableList<HandlerMethodArgumentResolver>) {
-
+        resolvers.add(userArgumentResolver)
     }
 }
