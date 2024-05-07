a4e9fc96572a8d5d2a56751e2513cfad007f1aba
BSpoones <ben.soones@gmail.com>
2024-05-07 19:07:10 +0100

    fix: naming

--- src/main/kotlin/cmp2804/tse/server/dto/DoctorSearchDTO.kt
+++ src/main/kotlin/cmp2804/tse/server/dto/DoctorSearchDTO.kt
@@ -3,7 +3,6 @@ package cmp2804.tse.server.dto
 data class DoctorSearchDTO(
     val postcode: String?,
     val range: Int,
-    val symptomIds: Set<Long>,
+    val symptoms: Set<Long>,
     val limit: Int
-
 )
