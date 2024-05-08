038d62061027cbd1f3108e846647f092196ed659
BSpoones <ben.soones@gmail.com>
2024-05-09 00:09:31 +0100

    add: Error docs

--- src/main/kotlin/cmp2804/tse/server/util/error/errors/EntityNotFoundException.kt
+++ src/main/kotlin/cmp2804/tse/server/util/error/errors/EntityNotFoundException.kt
@@ -1,9 +1,13 @@
 package cmp2804.tse.server.util.error.errors
 
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.ResponseStatus
+
 /**
  * Exception can be called whenever a request is made to a database
  * where expected data is not present.
  *
  * @author Ben Soones
  */
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
 class EntityNotFoundException(message: String? = null): Exception(message)
