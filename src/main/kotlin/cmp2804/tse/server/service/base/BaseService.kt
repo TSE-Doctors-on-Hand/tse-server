1fafda5fe84f3f3857da91a62db983a85c2e5c07
BSpoones <ben.soones@gmail.com>
2024-05-09 00:06:36 +0100

    add: Base service docs

--- src/main/kotlin/cmp2804/tse/server/service/base/BaseService.kt
+++ src/main/kotlin/cmp2804/tse/server/service/base/BaseService.kt
@@ -3,8 +3,18 @@ package cmp2804.tse.server.service.base
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.repository.findByIdOrNull
 
+/**
+ * Generic interface for Services
+ *
+ * This is responsible for functional integrity for all services
+ *
+ * @author Ben Soones
+ */
 interface BaseService<T: Any, ID> {
 
+    /**
+     * Returns the primary repository of the service
+     */
     fun getRepository(): JpaRepository<T, ID>
 
 }
