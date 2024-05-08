36ccd3408cb0de31a14c91f50a849692ee495e76
BSpoones <ben.soones@gmail.com>
2024-05-09 00:09:51 +0100

    add: Cartesian Point docs

--- src/main/kotlin/cmp2804/tse/server/util/matcher/CartesianPoint.kt
+++ src/main/kotlin/cmp2804/tse/server/util/matcher/CartesianPoint.kt
@@ -5,12 +5,23 @@ import cmp2804.tse.server.util.LatLong
 import kotlin.math.cos
 import kotlin.math.sin
 
+/**
+ * Represents a 3D Cartesian point on a spheroid (The Earth)
+ *
+ * @author Ben Soones
+ */
 data class CartesianPoint(
     val x: Double,
     val y: Double,
     val z: Double
 ) {
     companion object {
+
+        /**
+         * Calculating a cartesian point from a LatLong via spherical-to-Cartesian-transformation
+         *
+         * @see <a href="https://www.movable-type.co.uk/scripts/latlong.html">Source</a>
+         */
         fun fromLatLong(latLong: LatLong): CartesianPoint {
             val latRad = Math.toRadians(latLong.latitude)
             val longRad = Math.toRadians(latLong.longitude)
@@ -20,12 +31,14 @@ data class CartesianPoint(
                 y = EARTH_RADIUS_KM * cos(latRad) * sin(longRad),
                 z = EARTH_RADIUS_KM * sin(latRad)
             )
-
         }
     }
 
 }
 
+/**
+ * Calculates the dot product of two cartesian points
+ */
 fun dotProduct(
     point1: CartesianPoint,
     point2: CartesianPoint
