58556f95a755a5e9c940faab9bfaf389bf5b338a
BSpoones <ben.soones@gmail.com>
2024-05-09 00:09:07 +0100

    add: Exception Config docs

--- src/main/kotlin/cmp2804/tse/server/util/config/ExceptionConfig.kt
+++ src/main/kotlin/cmp2804/tse/server/util/config/ExceptionConfig.kt
@@ -4,9 +4,14 @@ import cmp2804.tse.server.util.error.GlobalExceptionHandler
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
+/**
+ * Configuration to allow for custom error handling
+ *
+ * @see [GlobalExceptionHandler]
+ * @author Ben Soones
+ */
 @Configuration
 class ExceptionConfig {
-
     @Bean
     fun globalExceptionHandler(): GlobalExceptionHandler = GlobalExceptionHandler()
 
