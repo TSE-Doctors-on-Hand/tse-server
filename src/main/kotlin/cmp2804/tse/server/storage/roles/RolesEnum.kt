aadd31cfd84c1c013ce0cf1911d38472fc574e4e
BSpoones <ben.soones@gmail.com>
2024-05-09 00:08:14 +0100

    add: Role Enum docs

--- src/main/kotlin/cmp2804/tse/server/storage/roles/RolesEnum.kt
+++ src/main/kotlin/cmp2804/tse/server/storage/roles/RolesEnum.kt
@@ -1,5 +1,13 @@
 package cmp2804.tse.server.storage.roles
 
+
+/**
+ * **Role Enum**
+ *
+ * Possible roles a user can have
+ * @see [cmp2804.tse.server.storage.users.User]
+ * @author Ben Soones
+ */
 enum class RolesEnum {
     PATIENT,
     DOCTOR,
