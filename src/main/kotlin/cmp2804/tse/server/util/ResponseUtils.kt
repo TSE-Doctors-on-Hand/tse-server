302008b7b47021fd516aa23652296219bead393c
BSpoones <ben.soones@gmail.com>
2024-05-09 00:10:07 +0100

    add: Response Utils docs

--- src/main/kotlin/cmp2804/tse/server/util/ResponseUtils.kt
+++ src/main/kotlin/cmp2804/tse/server/util/ResponseUtils.kt
@@ -6,7 +6,12 @@ import org.springframework.http.ResponseEntity
 
 object ResponseUtils {
 
-    // TODO -> Javadoc
+    /**
+     * Generic function to retrieve an entry in any Base service by its ID
+     *
+     * @return [ResponseEntity] of the same type as the service
+     * @author Ben Soones
+     */
     fun <T : Any> getEntryResponseById(service: BaseService<T, Long>, id: Long): ResponseEntity<T> {
         return service.getRepository().findById(id).map { ResponseEntity.ok(it) }
             .orElse(ResponseEntity.notFound().build())
