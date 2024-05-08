ee4e7cc855e1c8251d5191c053822190baebd5b5
BSpoones <ben.soones@gmail.com>
2024-05-09 00:08:38 +0100

    add: Sex Enum docs

--- src/main/kotlin/cmp2804/tse/server/storage/users/SexEnum.kt
+++ src/main/kotlin/cmp2804/tse/server/storage/users/SexEnum.kt
@@ -1,6 +1,13 @@
 package cmp2804.tse.server.storage.users
 
-enum class SexEnum {
+/**
+ * **SexEnum**
+ *
+ * Possible sexes a user can have
+ * @see [cmp2804.tse.server.storage.users.User]
+ * @author Ben Soones
+ */
+enum class SexEnum { // Yes, we broke naming conventions. In our defence, "sexy num" is funny
     MALE,
     FEMALE
 }
