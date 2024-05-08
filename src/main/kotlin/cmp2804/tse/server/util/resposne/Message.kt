d6ec7c9c7714ebcd6f86b612f93b57a7ce1452f6
BSpoones <ben.soones@gmail.com>
2024-05-09 00:10:12 +0100

    add: Message docs

--- src/main/kotlin/cmp2804/tse/server/util/resposne/Message.kt
+++ src/main/kotlin/cmp2804/tse/server/util/resposne/Message.kt
@@ -1,3 +1,10 @@
 package cmp2804.tse.server.util.resposne
 
+/**
+ * Message dataclasss
+ *
+ * This is used for all response messages
+ *
+ * @author Ben Soones
+ */
 data class Message(val message: String)
