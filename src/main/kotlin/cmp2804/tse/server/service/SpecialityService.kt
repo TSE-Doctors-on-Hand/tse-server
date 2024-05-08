cb3c44f7d3505880fef8de2c0b1ba865b68da4bc
BSpoones <ben.soones@gmail.com>
2024-05-09 00:07:08 +0100

    add: Speciality service docs

--- src/main/kotlin/cmp2804/tse/server/service/SpecialityService.kt
+++ src/main/kotlin/cmp2804/tse/server/service/SpecialityService.kt
@@ -4,14 +4,24 @@ import cmp2804.tse.server.service.base.BaseService
 import cmp2804.tse.server.storage.specialties.Speciality
 import cmp2804.tse.server.storage.specialties.SpecialtiesRepository
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 
+/**
+ * Specialty service
+ *
+ * This service is responsible for all functions interacting with specialties
+ *
+ * @author Ben Soones
+ * @author Oliver Whitehead
+ */
 @Service
 class SpecialityService(
     private val specialityRepository: SpecialtiesRepository
 ): BaseService<Speciality, Long> {
 
+    /**
+     * Inserts all mock specialties, This function will not be used in the live version
+     */
     fun insertSpecialities(specialities: List<Speciality>): Boolean {
         return try {
             specialities.forEach { speciality ->
